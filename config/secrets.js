c83b0c2bb3c1d81d12306be9f6540c06d7cb614e
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2015-06-20 19:54:33 +0200

    yolo secrets

--- config/secrets.js
+++ config/secrets.js
@@ -21,46 +21,47 @@
 
 module.exports = {
 
-  db: process.env.MONGOLAB_URI || process.env.MONGODB || '',
+  db: process.env.MONGOLAB_URI,
 
-  sessionSecret: process.env.SESSION_SECRET || '',
+  sessionSecret: process.env.SESSION_SECRET,
 
   sendgrid: {
-    user: process.env.SENDGRID_USER || '',
-    password: process.env.SENDGRID_PASSWORD || ''
+    user: process.env.SENDGRID_USER,
+    password: process.env.SENDGRID_PASSWORD,
   },
 
   facebook: {
-    clientID: process.env.FACEBOOK_ID || '',
-    clientSecret: process.env.FACEBOOK_SECRET || '',
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
     callbackURL: '/auth/facebook/callback',
     passReqToCallback: true
   },
 
   twitter: {
-    consumerKey: process.env.TWITTER_KEY || '',
-    consumerSecret: process.env.TWITTER_SECRET  || '',
+    consumerKey: process.env.TWITTER_KEY,
+    consumerSecret: process.env.TWITTER_SECRET ,
     callbackURL: '/auth/twitter/callback',
     passReqToCallback: true
   },
 
   google: {
-    clientID: process.env.GOOGLE_ID || '',
-    clientSecret: process.env.GOOGLE_SECRET || '',
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: '/auth/google/callback',
     passReqToCallback: true
   },
 
   twilio: {
-    sid: process.env.TWILIO_SID || '',
-    token: process.env.TWILIO_TOKEN || ''
+    sid: process.env.TWILIO_SID,
+    token: process.env.TWILIO_TOKEN,
   },
 
   paypal: {
-    host: 'api.sandbox.paypal.com',
-    client_id: process.env.PAYPAL_ID || '',
-    client_secret: process.env.PAYPAL_SECRET || '',
-    returnUrl: process.env.PAYPAL_RETURN_URL || 'http://localhost:3000/api/paypal/success',
-    cancelUrl: process.env.PAYPAL_CANCEL_URL || 'http://localhost:3000/api/paypal/cancel'
+    sandbox: true,
+    clientID: process.env.PAYPAL_KEY,
+    clientSecret: process.env.PAYPAL_SECRET,
+    callbackURL: '/auth/paypal/callback',
+    returnUrl: process.env.PAYPAL_RETURN_URL,
+    cancelUrl: process.env.PAYPAL_CANCEL_URL,
   }
 };
