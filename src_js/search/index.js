d82a23c1eedbd9cb2ca7c9479b1316d2da10fc93
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2015-06-20 22:54:58 +0200

    things

--- src_js/search/index.js
+++ src_js/search/index.js
@@ -1,3 +1,28 @@
 import $ from 'jquery';
+import Bacon from 'baconjs';
+$.fn.asEventStream = Bacon.$.asEventStream;
 
-const $search = $( '.js-recipe--search' );
+const $searchTextArea = $( '.js-recipe--search' );
+const $searchButton = $( '.js-recipe--search--button' );
+const $searchAdvanced = $( '.js-recipe--search--advanced' );
+
+const throttleSearch = $searchTextArea.asEventStream('keyup')
+    .map( ev => ev.target.value? ev.target.value.trim() : '' )
+    .filter( text => text.length > 2 )
+    .throttle( 500 )
+    .skipDuplicates();
+
+const searchApi = term => Bacon.fromPromise( $.ajax(`/api/recipes?q=${term}`) );
+
+const suggestions = throttleSearch.flatMapLatest( searchApi );
+
+const subscription = suggestions.subscribe(
+    data =>
+    {
+        console.log( data );
+    },
+    error =>
+    {
+        console.log( error );
+    }
+);
