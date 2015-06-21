054a1e82ceb82b7e210e24996b61cbcaa1138e61
Mouse Braun <mouse@knoblau.ch>
2015-06-21 08:53:55 +0200

    tech slide added

--- app.js
+++ app.js
@@ -123,6 +123,7 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
  * Primary app routes.
  */
 app.get('/', homeController.index);
+app.get('/technologies', function(req, res) { res.render( 'technologies' ); } );
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
