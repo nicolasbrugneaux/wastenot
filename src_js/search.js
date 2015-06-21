4519c165879775c86a51bdeef598bd300f45b813
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2015-06-21 04:06:43 +0200

    checkbox options

--- src_js/search.js
+++ src_js/search.js
@@ -4,6 +4,7 @@ const $searchAudio = $( '.js-recipe--search--audio' );
 const $searchTextArea = $( '.js-recipe--search' );
 const $searchButton = $( '.js-recipe--search--button' );
 const $searchAdvanced = $( '.js-recipe--search--advanced' );
+const $searchOptions = $( '.js-recipe--search--advanced-options' );
 
 const throttleSearch = $searchTextArea.asEventStream( 'keyup change' )
     .map( ev => ev.target.value? ev.target.value.trim().toLowerCase() : '' )
@@ -11,14 +12,23 @@ const throttleSearch = $searchTextArea.asEventStream( 'keyup change' )
     .throttle( 500 )
     .skipDuplicates();
 
-const searchApi = term => Bacon.fromPromise( $.ajax(`/api/recipes?q=${term}`) );
+const searchApi = term =>
+{
+    let options = '';
+    $searchOptions.find( 'input:checked' ).each( ( _, elm ) =>
+    {
+        options += '&' + encodeURIComponent( elm.value );
+    } );
+
+    return Bacon.fromPromise( $.ajax( `/api/recipes?q=${term}${options}` ) );
+};
 
 const suggestions = throttleSearch.flatMapLatest( searchApi );
 
 const subscription = suggestions.subscribe(
     data =>
     {
-        console.log( data );
+        console.log( JSON.parse( data.value() ) );
     },
     error =>
     {
@@ -27,3 +37,5 @@ const subscription = suggestions.subscribe(
 );
 
 $searchAudio.bind( 'click', speechListener );
+
+$searchAdvanced.bind( 'click', e => $searchOptions.toggleClass('hidden') );
